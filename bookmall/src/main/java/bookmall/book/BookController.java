3da1baafdaba31c1ddc4532d62b3efdd8ff0f116
k1mhyuns1k <1541_123@naver.com>
2022-02-07 11:37:08 +0900

    conflict error 해결 (도서 최종)

--- bookmall/src/main/java/bookmall/book/BookController.java
+++ bookmall/src/main/java/bookmall/book/BookController.java
@@ -1,15 +1,19 @@
 package bookmall.book;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mysql.cj.util.StringUtils;
+
+import bookmall.util.CommonUtil;
 
 @Controller
 public class BookController {
@@ -17,64 +21,99 @@ public class BookController {
 	@Autowired
 	BookService bookService;
 	
-	// 국내도서 페이지
-	@GetMapping("/book/korean.do")
-	public String korean(Model model, HttpServletRequest req, BookVo vo) {
-		List<BookVo> list = bookService.selectList(vo);
-		model.addAttribute("list", list);
-		return "book/koreanbooklist";
+	// 도서메인 페이지
+	@GetMapping("/book/mainbook.do")
+	public String korean(Model model, HttpServletRequest req, BookVo vo,
+						@RequestParam(value="bc") int classify) {
+		
+		vo.setClassify(classify);
+		vo.setBclassify(classify);
+		
+		List<BookVo> list = bookService.selectNewBook(vo);
+		List<BookVo> data = bookService.selectHotBook(vo);
+		List<BookVo> cate = bookService.cateNameList(vo);
+		
+		model.addAttribute("newBook", list);
+		model.addAttribute("hotBook", data);
+		model.addAttribute("cate", cate);
+		
+		return "book/mainbook";
 	}
 	
-	// 국외도서 페이지
-	@GetMapping("/book/abroad.do")
-	public String abroad() {
-		return "book/abroadbooklist";
-	}
-	
-	// 국내도서목록 페이지
-	@GetMapping("/book/koreanbooklist.do")
-	public String koreanbooklist(Model model, HttpServletRequest req, BookVo vo) {
-		List<BookVo> list = bookService.selectList(vo);
+	// 도서목록 페이지
+	@GetMapping("/book/booklist.do")
+	public String koreanbooklist(Model model, HttpServletRequest req, BookVo vo,
+								@RequestParam(value="bc") int classify,
+								@RequestParam(value="bc") int bclassify,
+								@RequestParam(value="blev") int blevel,
+								@RequestParam(value="cano") int bcategoryno,
+								@RequestParam(value="listOrder", required = false) String listOrder,
+								@RequestParam(required = false) Integer page,
+								@RequestParam(required = false) Integer numchoose,
+								@RequestParam(required = false) String searchWord) {
+		
+		if(page != null) {
+			vo.setPage(page);
+			vo.setNumchoose(numchoose);
+		}
+		
+		vo.setClassify(classify);
+		vo.setBclassify(bclassify);
+		vo.setBlevel(blevel);
+		vo.setBcategoryno(bcategoryno);
+		
+		if(searchWord != null) {
+			vo.setSearchWord(searchWord);
+		}
+		
+		// 총 갯수
+		int totCount = bookService.bookCount(vo); 
+		// 총 페이지 수
+		int totPage = totCount / vo.getNumchoose(); // ex) 31개면 3페이지
+		if (totCount % vo.getNumchoose() > 0) totPage++; //나눠서 나머지 있으면 1페이지 추가
+		//시작 인덱스 값 구하기, 10개씩 나오게, 1페이지 0~9, 2페이지 10~19
+		int startIdx = (vo.getPage()-1) * vo.getNumchoose(); //초기값 1이므로 0
+		
+		vo.setStartIdx(startIdx);
+		
+		
+		if(!StringUtils.isNullOrEmpty(listOrder)) {
+			vo.setListOrder(listOrder);
+		}
+		
+		List<BookVo> list = new ArrayList<BookVo>();
+		
+		if(totCount > 0) {
+			list = bookService.selectList(vo);
+		} 
+		
+		List<BookVo> cate = bookService.cateNameList(vo);
+		
 		model.addAttribute("list", list);
+		model.addAttribute("cate", cate);
+		model.addAttribute("param", vo);
+		model.addAttribute("totPage", totPage);	
+		model.addAttribute("totCount", totCount);
+		model.addAttribute("pageArea", CommonUtil.getBookListPageArea("booklist.do", vo.getPage(), totPage, vo.getNumchoose(), vo));
+		
 		return "book/booklist";
 	}
 	
-	// 국외도서목록 페이지
-	@GetMapping("/book/abroadbooklist.do")
-	public String abroadbooklist(Model model, HttpServletRequest req, BookVo vo) {
-		List<BookVo> list = bookService.selectList(vo);
-		model.addAttribute("list", list);
-		return "book/booklist";
-	}
-	
+
 	// 도서 상세 페이지
 	@GetMapping("/book/detail.do")
 	public String detail(Model model, @RequestParam int bookno) {
+
 		model.addAttribute("data", bookService.selectOne(bookno));
 		model.addAttribute("list", bookService.selectCate(bookno));
 		return "book/book_detail";
 	}
 	
-	// 국내도서 목록 - 카테고리
-	@GetMapping("/book/selectCateList.do")
-	@ResponseBody // 데이터를 반환해주는 어노테이션
-	public List<HashMap<String, Object>> selectCateList(HttpServletRequest req) {
-		
-		int bclassify = Integer.parseInt(req.getParameter("bclassify"));
-		int blevel = 0;
-		int bcategoryno = 0;
-		
-		if(!StringUtils.isEmpty(req.getParameter("blevel"))) {
-			blevel = Integer.parseInt(req.getParameter("blevel"));
-		}
-		
-		if(!StringUtils.isEmpty(req.getParameter("bcategoryno"))) {
-			bcategoryno = Integer.parseInt(req.getParameter("bcategoryno"));
-		}
-		
-		List<HashMap<String, Object>> list = bookService.selectCateList(bclassify, blevel, bcategoryno);
-
-		return list;
-		
+	// 카테고리 바
+	@GetMapping("/book/category.do")
+	public String category(Model model, HttpServletRequest req, BookVo vo) {
+		List<BookVo> cate = bookService.cateNameList(vo);
+		model.addAttribute("cate", cate);
+		return "book/category";
 	}
 }
--- bookmall/src/main/webapp/WEB-INF/view/book/category.jsp
+++ bookmall/src/main/webapp/WEB-INF/view/book/category.jsp
@@ -1,29 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>해외도서</title>
-    
-    <!-- css -->
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
-    <link rel="stylesheet" href="/bookmall/css/reset.css"/>
-    <link rel="stylesheet" href="/bookmall/css/com.css"/>
-    
-    <!-- js -->
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
-    <script src="/bookmall/js/common.js"></script>
-    <script src="/bookmall/js/main.js"></script>
 
 <c:set var="oldnum" value="" />
 <div class="nav_category">
 	<c:forEach var="vo" items="${cate}" varStatus="status">
+	
 	<c:if test="${oldnum == ''}">
 		<a href="/bookmall/book/mainbook.do?bc=0">국내도서</a>
 	</c:if>
@@ -48,4 +30,4 @@
 		</c:if>
 		<c:set var="oldnum" value="${vo.blev1 }" />
 	</c:forEach>
-	</div>
+</div>
